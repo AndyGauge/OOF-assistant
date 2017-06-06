01aadac39d5db52c8e0ed1b98243d6ec2db1aaf1
Andy Gauge <andygauge@gmail.com>
2017-06-06 16:20:27 -0700

    retrieve last mailbox message

--- app/models/mailbox.rb
+++ app/models/mailbox.rb
@@ -2,7 +2,7 @@ class Mailbox < ApplicationRecord
   belongs_to :last_message, class_name: 'Message', optional: true
 
   def last_mailbox_message
-    MailboxMessage.find_by(mailbox: self, message: last_message) if last_message
+    MailboxMessage.where(mailbox: self, message: last_message).order(created_at: :desc).first if last_message
   end
 
   def set_oof_with_new_message(message)
